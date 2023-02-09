c88f4628b54f9afd93ab881b0bd5ead73407acc9
rin4th <rizalnur.art@gmail.com>
2023-02-09 21:40:13 +0700

    Final Footer and add data/sites

---
+++ src/data/sites.ts
@@ -0,0 +1,9 @@
+export default {
+  siteName: "Rin4th",
+  siteUrl: "https://localhost:3000",
+  github: "https://github.com/rin4th",
+  twitter: "https://twitter.com/",
+  email: "rizalnur.art@gmail.com",
+  description: "Rin4th's personal website",
+  keywords: ["personal", "website", "blog", "article"],
+} as const;
